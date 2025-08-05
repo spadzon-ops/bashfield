3fcfb32a6a0a8dd8826562cf5e6cf2c71d6c28df
spadzon-ops <spadzon@gmail.com>
2025-08-05 03:11:37 +0300

    Update _app.js

--- bashfield/pages/_app.js
+++ bashfield/pages/_app.js
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import { appWithTranslation } from 'next-i18next'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
 function MyApp({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionContextProvider>
   )
 }
 
