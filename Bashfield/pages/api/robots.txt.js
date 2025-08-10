7ac983a9ee0d2210845ff8a23c889f94f6c62f39
spadzon-ops <spadzon@gmail.com>
2025-08-11 00:46:01 +0300

    Delete Bashfield directory

--- Bashfield/pages/api/robots.txt.js
+++
@@ -1,9 +0,0 @@
-
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(`User-agent: *
-Allow: /
-
-Sitemap: ${process.env.NEXT_PUBLIC_SITE_URL || ''}/api/sitemap.xml
-`);
-}
