443872c6ca7f28fdfde428c22583f75855d9fabc
spadzon-ops <spadzon@gmail.com>
2025-08-11 23:22:53 +0300

    Add files via upload

--- bashfield/next-i18next.config.js
+++ bashfield/next-i18next.config.js
@@ -1,10 +1,6 @@
-// next-i18next.config.js
 module.exports = {
   i18n: {
-    locales: ['en', 'fr'],
     defaultLocale: 'en',
-  },
-  backend: {
-    loadPath: '/locales/{{lng}}/{{ns}}.json',
+    locales: ['en', 'ku', 'ar'],
   },
 }
