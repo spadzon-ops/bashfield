997b69881c8f90c8273c06af05b9e6f2ba5b3b9a
spadzon-ops <spadzon@gmail.com>
2025-08-11 09:20:09 +0300

    Add files via upload

--- bashfield/pages/_app.js
+++ bashfield/pages/_app.js
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import { appWithTranslation } from 'next-i18next'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { createBrowserClient } from '@supabase/ssr'
 
 function MyApp({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+  const [supabase] = useState(() => 
+    createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    )
+  )
 
   return (
-    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionContextProvider>
+    <Layout>
+      <Component {...pageProps} supabase={supabase} />
+    </Layout>
   )
 }
 
