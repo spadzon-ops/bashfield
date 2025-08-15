30ca7b0a9debc3fc9086936f5243a450d0bf3806
spadzon-ops <spadzon@gmail.com>
2025-08-15 15:28:58 +0300

    Delete bashfield directory

--- bashfield/add-size-column.sql
+++
@@ -1,5 +0,0 @@
--- Add size_sqm column to listings table
-ALTER TABLE public.listings ADD COLUMN IF NOT EXISTS size_sqm INTEGER;
-
--- Add comment to the column
-COMMENT ON COLUMN public.listings.size_sqm IS 'Property size in square meters';
