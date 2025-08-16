24ac2165ac4e342e982207cc106aecb611598ae6
spadzon-ops <spadzon@gmail.com>
2025-08-16 16:08:33 +0300

    Update useComprehensiveTranslations.js

--- bashfield/hooks/useComprehensiveTranslations.js
+++ bashfield/hooks/useComprehensiveTranslations.js
@@ -1,25 +1,6 @@
 // bashfield/hooks/useComprehensiveTranslations.js
-import { useState, useEffect } from 'react'
-import i18n from '../lib/i18n-lite'
-
+import { useTranslation } from '../contexts/TranslationContext'
 export default function useComprehensiveTranslations() {
-  const [currentLang, setCurrentLang] = useState('en')
-
-  useEffect(() => {
-    const lang = i18n.getLang()
-    setCurrentLang(lang)
-    i18n.applyDocumentDirection(lang)
-    i18n.translatePage(lang)
-
-    const onChange = (e) => {
-      const l = e?.detail?.language || i18n.getLang()
-      setCurrentLang(l)
-      i18n.applyDocumentDirection(l)
-      i18n.translatePage(l)
-    }
-    window.addEventListener('languageChanged', onChange)
-    return () => window.removeEventListener('languageChanged', onChange)
-  }, [])
-
-  return { t: (key) => i18n.t(key, currentLang), currentLang }
+  const { t, lang } = useTranslation()
+  return { t, currentLang: lang }
 }
