b06592b37d0d7c23ace0e823f470166e96718650
spadzon-ops <spadzon@gmail.com>
2025-08-16 16:57:56 +0300

    Update _app.js

--- bashfield/pages/_app.js
+++ bashfield/pages/_app.js
@@ -1,38 +1,11 @@
-// bashfield/pages/_app.js
-import Head from 'next/head'
-import { useState } from 'react'
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { ModeProvider } from '../contexts/ModeContext'
-import { TranslationProvider } from '../contexts/TranslationContext'
-import Layout from '../components/Layout'
-import MessagesLayout from '../components/MessagesLayout'
-import { useRouter } from 'next/router'
-import '../styles/globals.css'
+import { useRouter } from 'next/router';
+import { TranslationProvider } from '../contexts/TranslationContext';
 
-function MyApp({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createPagesBrowserClient())
-  const router = useRouter()
-  const isMessagesRoute = router.pathname.startsWith('/messages') || router.pathname.startsWith('/chat/')
-  const LayoutComponent = isMessagesRoute ? MessagesLayout : Layout
-
+export default function App({ Component, pageProps }) {
+  const { locale } = useRouter();
   return (
-    <>
-      <Head>
-        <title>Bashfield</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-        <TranslationProvider initialLang="en">
-          <ModeProvider>
-            <LayoutComponent>
-              <Component {...pageProps} />
-            </LayoutComponent>
-          </ModeProvider>
-        </TranslationProvider>
-      </SessionContextProvider>
-    </>
-  )
+    <TranslationProvider lang={locale || 'en'}>
+      <Component {...pageProps} />
+    </TranslationProvider>
+  );
 }
-
-export default MyApp
