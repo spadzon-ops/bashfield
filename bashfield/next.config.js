64e88140548fb65066a0e68f47511028e1e56a31
spadzon-ops <spadzon@gmail.com>
2025-08-16 17:13:57 +0300

    Update next.config.js

--- bashfield/next.config.js
+++ bashfield/next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   i18n: {
     locales: ['en', 'ar', 'ku'],
     defaultLocale: 'en',
   },
   reactStrictMode: true,
+  webpack: (config) => {
+    // Route all next-i18next imports to our shims
+    config.resolve.alias['next-i18next'] = path.resolve(__dirname, 'lib/i18n-shim.js');
+    config.resolve.alias['next-i18next/serverSideTranslations'] = path.resolve(
+      __dirname,
+      'lib/serverSideTranslations-shim.js'
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
