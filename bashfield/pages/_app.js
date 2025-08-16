5c215d730dfb161ebb29ea2188df50031158f49f
spadzon-ops <spadzon@gmail.com>
2025-08-16 17:40:21 +0300

    Update _app.js

--- bashfield/pages/_app.js
+++ bashfield/pages/_app.js
@@ -1,13 +1,8 @@
-// pages/_app.js
-import { TranslationProvider } from "../contexts/TranslationContext";
-import "../styles/globals.css"; // keep if you have it; safe to leave even if missing
+import '@/styles/globals.css';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <TranslationProvider initialLanguage="en">
-      <Component {...pageProps} />
-    </TranslationProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
