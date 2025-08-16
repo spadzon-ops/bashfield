990737c09c9f32523e7ba5bb01f048591eafb08c
spadzon-ops <spadzon@gmail.com>
2025-08-16 17:56:18 +0300

    Update _app.js

--- bashfield/pages/_app.js
+++ bashfield/pages/_app.js
@@ -1,8 +1,15 @@
+// pages/_app.js
 import '@/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
+import { TranslationProvider } from '@/contexts/TranslationContext';
+import nextI18NextConfig from '@/../next-i18next.config.js';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <TranslationProvider>
+      <Component {...pageProps} />
+    </TranslationProvider>
+  );
 }
 
-export default appWithTranslation(MyApp);
+export default appWithTranslation(App, nextI18NextConfig);
