fcc77b33b30c4c9d0e8d548957fb0be9ac146cd3
spadzon-ops <spadzon@gmail.com>
2025-08-16 17:18:34 +0300

    Update _app.js

--- bashfield/pages/_app.js
+++ bashfield/pages/_app.js
@@ -1,9 +1,10 @@
-import '@/styles/globals.css';
-import { TranslationProvider } from '../contexts/TranslationContext';
+// pages/_app.js
+import { TranslationProvider } from "../contexts/TranslationContext";
+import "../styles/globals.css"; // keep if you have it; safe to leave even if missing
 
 function MyApp({ Component, pageProps }) {
   return (
-    <TranslationProvider>
+    <TranslationProvider initialLanguage="en">
       <Component {...pageProps} />
     </TranslationProvider>
   );
