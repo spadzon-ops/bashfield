2d0859db2403b72975242501e8dca42e8ba0bae9
spadzon-ops <spadzon@gmail.com>
2025-08-16 18:12:20 +0300

    Create _document.js

---
+++ bashfield/pages/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    // Treat Arabic + Kurdish as RTL; English as LTR
+    const locale = this.props.__NEXT_DATA__?.locale || 'en';
+    const rtl = ['ar', 'ku'].includes(locale);
+    return (
+      <Html lang={locale} dir={rtl ? 'rtl' : 'ltr'}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
