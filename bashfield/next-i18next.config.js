09858d068afb1834b24c0a5d69a5e28d95efe15d
spadzon-ops <spadzon@gmail.com>
2025-08-18 22:08:13 +0300

    Add files via upload

---
+++ bashfield/next-i18next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ku', 'ar'],
+    localeDetection: false,
+  },
+  fallbackLng: 'en',
+  debug: false,
+  reloadOnPrerender: false,
+}
