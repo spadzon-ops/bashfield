f6edf520015563a5b4e9a1e85561ed5b16989bba
spadzon-ops <spadzon@gmail.com>
2025-08-18 20:59:07 +0300

    Update next-i18next.config.js

--- bashfield/next-i18next.config.js
+++ bashfield/next-i18next.config.js
@@ -3,4 +3,7 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'ku', 'ar'],
   },
+  fallbackLng: 'en',
+  debug: false,
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
